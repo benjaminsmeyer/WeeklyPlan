e6c71b59841ffcd8e77dbc0dcb6baa4c37eed0a4
Ben Meyer <88737362+benjaminsmeyer@users.noreply.github.com>
2023-06-14 15:18:55 -0400

    Created Activity Class w/ Event & Task

---
+++ src/main/java/cs3500/pa05/Driver.java
@@ -0,0 +1,15 @@
+package cs3500.pa05;
+
+/**
+ * Runs the weekly planner
+ */
+public class Driver {
+  /**
+   * Runs the program
+   *
+   * @param args the arguments
+   */
+  public static void main(String[] args) {
+
+  }
+}
