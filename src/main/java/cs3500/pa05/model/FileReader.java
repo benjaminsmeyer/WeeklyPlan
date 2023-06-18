29eddc1a98e97607c2a0a9e8484f1c87b62ce1c5
Percy Masetti Zannini <masettizannini.a@northeastern.edu>
2023-06-18 15:36:52 -0400

    FileReader implemented (it's a little manual, check out more efficent way of doing it)

--- src/main/java/cs3500/pa05/model/FileReader.java
+++ src/main/java/cs3500/pa05/model/FileReader.java
@@ -1,30 +1,71 @@
 package cs3500.pa05.model;
 
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import cs3500.pa05.json.ActivityJson;
+import cs3500.pa05.json.DayJson;
+import cs3500.pa05.json.TextJson;
+import cs3500.pa05.json.WeekJson;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.nio.charset.StandardCharsets;
-import java.util.Scanner;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FileReader {
   //json file reader?
+  private File file;
+  private ObjectMapper mapper = new ObjectMapper();
+  List<Day> days;
+  String name;
+  int maxEvents;
+  int maxTasks;
+  String notes;
+  String quotes;
 
+  public void openFile(File file) {
+    this.file = file;
+  }
 
-
-  public String readFile(File file) {
-    Scanner sc;
-    try { // The file may not exist, in which case we need to handle that error (hence try-catch)
-      sc = new Scanner(new FileInputStream(file), StandardCharsets.UTF_8);
-    } catch (FileNotFoundException e) {
-      throw new RuntimeException(e);
+  public void readFile() {
+    try {
+      JsonParser parser = this.mapper.getFactory().createParser(file);
+      WeekJson message = parser.readValueAs(WeekJson.class);
+      delegateMessage(message);
+      //TODO: connect load to week gui
+    } catch (IOException e) {
+      // Disconnected from server or parsing exception
     }
+  }
 
-    StringBuilder content = new StringBuilder();
-    while (sc.hasNextLine()) { // Check there is another unread line in the file
-      String line = sc.nextLine();
-      content.append(line);
+  private void delegateMessage(WeekJson message) {
+    parseDayJson(message.days());
+    name = message.name();
+    maxEvents = message.maxEvents();
+    maxTasks = message.maxTasks();
+    parseTextJson(message.text());
+  }
+
+  private void parseDayJson(DayJson[] dayJsons) {
+    days = new ArrayList<>();
+    for (DayJson dayJson : dayJsons) {
+      days.add(new Day(dayJson.dayOfWeek(), parseSchedule(dayJson)));
     }
+  }
 
-    return content.toString();
+  private List<Activity> parseSchedule(DayJson dayJson) {
+    List<Activity> activities = new ArrayList<>();
+    for (ActivityJson activity : dayJson.schedule()) {
+      if (activity.isEvent()) {
+        activities.add(mapper.convertValue(activity.event(), Event.class));
+      } else {
+        activities.add(mapper.convertValue(activity.task(), Task.class));
+      }
+    }
+    return activities;
+  }
+
+  private void parseTextJson(TextJson textJson) {
+    quotes = textJson.quotes();
+    notes = textJson.notes();
   }
 }
