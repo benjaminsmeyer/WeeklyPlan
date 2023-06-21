a9938bf1cbb013d20c9d51db2db62c0734cd5d65
Ben Meyer <88737362+benjaminsmeyer@users.noreply.github.com>
2023-06-21 11:25:24 -0400

    Added more Save Tests

--- src/test/java/cs3500/pa05/model/SaveTest.java
+++ src/test/java/cs3500/pa05/model/SaveTest.java
@@ -3,18 +3,34 @@ package cs3500.pa05.model;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class SaveTest {
 
   @Test
   void saveWeekTest() {
+    List<Day> days = new ArrayList<>();
+
+    Day day = new Day(DayOfWeek.SUNDAY);
+    day.addActivity(new Task("task", "task description", DayOfWeek.SUNDAY));
+    day.addActivity(new Event("event", "event description", DayOfWeek.SUNDAY, 1000, 60));
+    days.add(day);
+
+    days.add(new Day(DayOfWeek.MONDAY));
+    days.add(new Day(DayOfWeek.TUESDAY));
+    days.add(new Day(DayOfWeek.WEDNESDAY));
+    days.add(new Day(DayOfWeek.THURSDAY));
+    days.add(new Day(DayOfWeek.FRIDAY));
+    days.add(new Day(DayOfWeek.SATURDAY));
+
+    Week week = new Week(days, "test week");
     Save save = new Save("src/test/java/cs3500/pa05/model/testfiles/weekTest.bujo");
-    Week week = new Week();
     save.saveWeek(week);
     FileReader fileReader = new FileReader();
     File file = new File("src/test/java/cs3500/pa05/model/testfiles/weekTest.bujo");
     fileReader.openFile(file);
-    assertEquals("My Week", fileReader.readFile().getName());
+    assertEquals("test week", fileReader.readFile().getName());
   }
 }
